3a34637b2466f08190d6e217297399dd1f0bae21
khadeom <khadeom10@gmail.com>
2023-01-17 23:36:33 +0530

    project inatialized

---
+++ apps/gulmohar/urls.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.urls import path
+from .views import newPage, landing3,landing4, landing5,landing6
+
+urlpatterns = [
+    path("new", newPage, name="newpage"),
+    path("landing3", landing3, name="landing3"),
+    path("landing4", landing4, name="landing4"),
+    path("landing5", landing5, name="landing5"),
+    path("landing6", landing6, name="landing6"),
+]
---
+++ apps/gulmohar/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def newPage(request):
+    return render(request, "gulmohar/landingPage.html")
+
+def landing3(request):
+    return render(request, "gulmohar/landing3.html")
+
+def landing4(request):
+    return render(request, "gulmohar/landing4.html")    
+def landing5(request):
+    return render(request, "gulmohar/landing5.html")
+
+def landing6(request):
+    return render(request, "gulmohar/landing6.html")
