96abd7b90aa7cdcceb8865d00e8a7d2ff92f122e
khadeom <khadeom10@gmail.com>
2023-02-03 22:56:26 +0530

    randome changes

--- core/settings.py
+++ core/settings.py
@@ -27,9 +27,7 @@ DEBUG = env('DEBUG')
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets') 
 
 # load production server from .env
-# ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1', 'gulmohar.azurewebsites.net',               env('SERVER', default='127.0.0.1') ]
-ALLOWED_HOSTS        = ['gulmohar.azurewebsites.net']
-
+ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1', 'gulmohar.azurewebsites.net',  env('SERVER', default='127.0.0.1') ]
 
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + env('SERVER', default='127.0.0.1') ]
